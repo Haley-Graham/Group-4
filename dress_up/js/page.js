0512229e3439203a1044d5445f2802d8f7f89f08
Aimee Castellon <70286071+abceemi@users.noreply.github.com>
2021-04-19 01:56:36 -0700

    Update page.js

--- dress_up/js/page.js
+++ dress_up/js/page.js
@@ -1,35 +1,66 @@
 window.onload=init;
-var state = {
-    i : 0
-};
 
 function init()
 {
     console.log("window has loaded");
     state.i=1;
-    
+    state.j=1;
+
 }
 
-function next_top()
+var state = {
+    i : 0,
+    j:0,
+    k:0
+};
+
+function nextshirt()
 {
-    console.log("inside function next_top");
+    console.log("inside function nextshirt");
     console.log(state.i);
-    var dress=document.getElementById("shirts");
+    var shirt=document.getElementById("clothes");
     if(state.i===0){
-    shirts.setAttribute("class","black_shirt");
+    shirt.setAttribute("class","shirt1");
         state.i++;
         console.log(state.i);
     }
     else
      if(state.i===1){
-    shirts.setAttribute("class","flower_shirt");
+    shirt.setAttribute("class","shirt2");
          state.i++;
          console.log(state.i);
     }
     else
      if(state.i===2){
-    shirts.setAttribute("class","flower_shirt");
+    shirt.setAttribute("class","shirt3");
          state.i=0;
     }
+
+}
+
+function nextpants()
+{
+    console.log("inside function nextpants");
+    console.log(state.j);
+    var pants=document.getElementById("pants");
+    if(state.j===0){
+    pants.setAttribute("class","pants1");
+        state.j++;
+        console.log(state.j);
+    }
+    else
+     if(state.j===1){
+    pants.setAttribute("class","pants2");
+         state.j++;
+         console.log(state.j);
+    }
+    else
+     if(state.j===2){
+    pants.setAttribute("class","pants3");
+         state.j=0;
+    }
+
+}
+
 
 }
