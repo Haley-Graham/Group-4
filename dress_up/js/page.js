1189df922778e2247176b55e489356cbb0cc0ffb
Ayline Castrejon <78106037+acnavarro98@users.noreply.github.com>
2021-05-10 11:40:11 -0700

    Update page.js

--- dress_up/js/page.js
+++ dress_up/js/page.js
@@ -1,51 +1,13 @@
-window.onload=init;
-
-function init() {
-    console.log("window has loaded");
-    state.i=1;
-}
-
-var state = {
-    i : 0,
-};
-
-function next_top() {
-    console.log("inside function next_top");
-    console.log(state.i);
-    var top=document.getElementById("clothes");
-    if(state.i===0){
-    top.setAttribute("class","pink_shirt");
-        state.i++;
-        console.log(state.i);
+$( function() {
+  $( "#draggable" ).draggable();
+  $( "#draggable1" ).draggable();
+  $( "#draggable2" ).draggable();
+  $( "#droppable" ).droppable({
+    drop: function( event, ui ) {
+      $( this )
+        .addClass( "ui-state-highlight" )
+        .find( "p" )
+          .html( "Dropped!" );
     }
-    else
-     if(state.i===1){
-    top.setAttribute("class","purple_shirt");
-         state.i++;
-         console.log(state.i);
-    }
-    else
-     if(state.i===2){
-    top.setAttribute("class","yellow_shirt");
-        state.i++;
-        console.log(state.i);
-    }
-    else
-     if(state.i===3){
-    top.setAttribute("class","black_shirt");
-        state.i++;
-        console.log(state.i);
-    }
-    else
-     if(state.i===4){
-    top.setAttribute("class","flower_shirt");
-        state.i++;
-        console.log(state.i);
-    }
-    else
-     if(state.i===5){
-    top.setAttribute("class","a_shirt");
-         state.i=0;
-    }
-
-}
+  });
+} );
