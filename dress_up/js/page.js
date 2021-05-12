a0661d7e11be1debb511ee5db4bdb38da89d402f
Haley-Graham <hagraham@csumb.edu>
2021-05-12 11:06:47 -0700

    added bottoms, fixed grid, made stuff snap

--- dress_up/js/page.js
+++ dress_up/js/page.js
@@ -1,7 +1,28 @@
 $( function() {
-  $( "#draggable" ).draggable();
-  $( "#draggable1" ).draggable();
-  $( "#draggable2" ).draggable();
+  $( "#draggable" ).draggable(
+    { snap: ".ui-widget-header", snapTolerance: "50"});
+  $( "#draggable1" ).draggable(
+    { snap: ".ui-widget-header", snapTolerance: "50"});
+  $( "#draggable2" ).draggable(
+    { snap: ".ui-widget-header", snapTolerance: "50"});
+  $( "#draggable3" ).draggable(
+    { snap: ".ui-widget-header", snapTolerance: "50"});
+  $( "#draggable4" ).draggable(
+    { snap: ".ui-widget-header", snapTolerance: "50"});
+  $( "#draggable5" ).draggable(
+    { snap: ".ui-widget-header", snapTolerance: "50"});
+  $( "#draggable6" ).draggable(
+    { snap: ".ui-widget-header", snapTolerance: "50"});
+  $( "#draggable7" ).draggable(
+    { snap: ".ui-widget-header", snapTolerance: "50"});
+  $( "#draggable8" ).draggable(
+    { snap: ".ui-widget-header", snapTolerance: "50"});
+  $( "#draggable9" ).draggable(
+    { snap: ".ui-widget-header", snapTolerance: "50"});
+  $( "#draggable10" ).draggable(
+    { snap: ".ui-widget-header", snapTolerance: "50"});
+  $( "#draggable11" ).draggable(
+    { snap: ".ui-widget-header", snapTolerance: "50"});
   $( "#droppable" ).droppable({
     drop: function( event, ui ) {
       $( this )
